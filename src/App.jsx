dce55ba85bb2ad634cb33dd8e229763c1c97ed63
steady-shunpo <shunpoguy@gmail.com>
2025-01-27 09:14:25 +0530

    added localsave

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from 'react';
 
 function App() {
   const [todo, setTodo] = useState("");
   const [listTodo, setListTodo] = useState([]);
 
+  useEffect(() => {
+    let todostring = localStorage.getItem("todos")
+    if(todostring){
+      let copytodos = JSON.parse(todostring)
+      console.log(copytodos)
+      setListTodo(copytodos)
+    }
+  }, [])
+  
+
+  
 
   const handleEdit = (e) => {
     let id = e.target.name;
@@ -19,30 +31,48 @@ function App() {
     })
     setListTodo(copytodos)
   }
+
+
   const handleDelete = (e) => {
     let id = e.target.name;
-    let copyTodo = [...listTodo];
+    setListTodo((prevList)=>{
+      let copyTodo = [...prevList];
 
-    let todoindex = copyTodo.findIndex(listtodo => listtodo.id == id);
-    copyTodo.splice(todoindex, 1)
-    setListTodo(copyTodo) 
-    
+      let todoindex = prevList.findIndex(listtodo => listtodo.id == id);
+      copyTodo.splice(todoindex, 1)
+      console.log(copyTodo)
+      localStorage.setItem("todos", JSON.stringify(copyTodo))
+      return copyTodo
+    }) 
   }
+
+
   const handleAdd = () => {
-    setListTodo([...listTodo, {id:uuidv4() ,todo, isCompleted: false }])
+    setListTodo((prevList)=>{
+      const newlist = [...prevList, {id:uuidv4() ,todo, isCompleted: false }]
+      console.log(newlist)
+      localStorage.setItem("todos", JSON.stringify(newlist))
+      return newlist
+    })
     setTodo("")
-    console.log(listTodo)
   }
+
+
   const handleChange = (e) => {
     setTodo(e.target.value);
   }
+
   
   const handleComplete = (e) => {
     let id = e.target.name;
-    let copyTodo = [...listTodo];
+    setListTodo((prevList) =>{
+      let copyTodo = [...prevList];
+      return copyTodo
+    }) 
+    let copyTodo = [...listTodo]
     let todoindex = copyTodo.findIndex(listtodo => listtodo.id == id);
     copyTodo[todoindex].isCompleted = !copyTodo[todoindex].isCompleted
-    setListTodo(copyTodo) 
+    localStorage.setItem("todos", JSON.stringify(copyTodo))
   }
 
 
@@ -53,7 +83,7 @@ function App() {
         <div className="text-center font-bold text-4xl mt-12">TASK MASTER</div>
         <div className="todoadder flex my-7 gap-3 justify-center mt-8 align-middle">
           <input onChange={handleChange} value={todo} type="text" className='w-1/3 rounded-lg h-9 bg-slate-900 opacity-60' />
-          <button onClick={handleAdd} className='bg-slate-900 h-9 px-4 rounded-lg'>Add</button>
+          <button onClick={handleAdd} disabled={todo.length<=1} className='bg-slate-900 h-9 px-4 rounded-lg'>Add</button>
         </div>
         <div className="datetime flex justify-center gap-2">
           <div className="date">21/1/25,</div>
@@ -64,7 +94,7 @@ function App() {
           {listTodo.map(item => {
             return <div key = {item.id} className="todos flex justify-center align-middle mt-5 gap-2">
 
-              <input type="checkbox" onChange={handleComplete} value={item.isCompleted} name={item.id} id="" />
+              <input type="checkbox" onChange={handleComplete} checked={item.isCompleted} name={item.id} id="" />
 
               <div className="text w-1/3 rounded-lg h-9 bg-slate-900 opacity-60 text-center">
                 
